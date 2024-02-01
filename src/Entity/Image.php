26a2508a40e5f89bcc5f4a576c3f7c9cc3c50482
hale0010 <nadhir.hales@etudiant.univ-reims.fr>
2024-02-01 17:22:57 +0100

    ajout Entité Image

---
+++ src/Entity/Image.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ImageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ImageRepository::class)]
+class Image
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $lien = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLien(): ?string
+    {
+        return $this->lien;
+    }
+
+    public function setLien(string $lien): static
+    {
+        $this->lien = $lien;
+
+        return $this;
+    }
+}
